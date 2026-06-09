5b1ca8060e3bab05646ca95d4ecb2203f396734f
Pétrus Blunk <tds742053@gmail.com>
2026-06-09 11:04:28 -0300

    c
    5

---
+++ idade.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+ int main(){
+
+       
+ printf("Qual a sua idade:  "):
+ scanf("%d"&idade)
+ printf(" Aidade informada foi: &d \n",
+      
+
+       
+     sytem("pause");
+     return 0;
+
+
+ }     
+
+
+
---
+++ soma.c
@@ -0,0 +1,27 @@
+//inclui a bibiblioteca stdio.h que permite usar entrada e saida de infos(printf e escanf)
+#inc<stdio.h>
+//inclui a bibiblioteca stdlib.h que permite funcoes utilitarias(system))
+#include<stdlib.h>
+//Declarar a funçao principal, opnde o programa começa a execução
+int main(){
+
+    //declarar variaveis
+    float numero1, numero2, respostaSoma;
+
+    print("Digite o primeiro numero: ");
+scanFe("%f" , &numero1);
+print("Digite o segundo numero: ");
+scanFe("%f" , &numero2);
+
+  respostaSoma=numero1=numero2
+  printf("%.2f\n",respostaSoma)
+  //pausar o sistema.
+   system("pause");
+
+   return
+
+
+
+
+}
+
